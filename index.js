05189a0cb83efc2cb3c232a10132727e00eab082
AnuragChoubey95 <anurag.choubey1995@gmail.com>
2025-01-21 13:07:27 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
@@ -11,99 +11,52 @@ app.use(cors())
 
 const morgan = require('morgan')
 morgan.token('body', (req) => {
-    return JSON.stringify(req.body); // Convert the body to a string
-  });
+  return JSON.stringify(req.body) // Convert the body to a string
+})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const Contact = require('./models/contacts')
-
-const name = "name";
-const number = "number";
-const id = "id";
-
-let persons = [
-  {
-    name: "Alan Turing",
-    number: "+1 132 654 9870",
-    id: "1"
-  },
-  {
-    name: "Grace Hopper",
-    number: "+1 555-123-4567",
-    id: "2"
-  },
-  {
-    name: "Marie Curie",
-    number: "987-654-3210",
-    id: "3"
-  },
-  {
-    name: "Nikola Tesla",
-    number: "+44 20 7946 0958",
-    id: "4"
-  },
-  {
-    name: "Charles Babbage",
-    number: "222-333-4444",
-    id: "5"
-  },
-  {
-    name: "Rosalind Franklin",
-    number: "+1 777-888-9999",
-    id: "6"
-  },
-  {
-    name: "Katherine Johnson",
-    number: "555-666-7777",
-    id: "7"
-  },
-  {
-    name: "Carl Sagan",
-    number: "+1 888-999-0000",
-    id: "8"
-  }
-] 
-  
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  Contact.find({}).then(contacts => {
-    if(contacts){
-      response.json(contacts)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => {
-    console.log(error)
-    response.status(500).end()
-  })
+  Contact.find({})
+    .then(contacts => {
+      if(contacts){
+        response.json(contacts)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
 })
 
 app.get('/info', (request, response, next) => {
   Contact.countDocuments({})
-      .then(count => {
-          const info = `Phonebook has info for ${count} people.`;
-          const date = new Date();
-          const formattedDate = date.toString();
+    .then(count => {
+      const info = `Phonebook has info for ${count} people.`
+      const date = new Date()
+      const formattedDate = date.toString()
 
-          const htmlResponse = `
+      const htmlResponse = `
               <body>
                   <p>${info}</p>
                   <p>${formattedDate}</p>
               </body>
-          `;
+          `
 
-          response.send(htmlResponse);
-      })
-      .catch(error => {
-          console.error('Error fetching data:', error);
-          next(error); 
-      });
-});
+      response.send(htmlResponse)
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error)
+      next(error)
+    })
+})
 
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -112,7 +65,7 @@ app.get('/api/persons/:id', (request, response, next) => {
       if (person) {
         response.json(person)
       } else {
-        response.status(404).end() 
+        response.status(404).end()
       }
     })
     .catch(error => next(error))
@@ -120,46 +73,42 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Contact.findByIdAndDelete(request.params.id)
-      .then(result => {
-          response.status(204).end()
-      })
-      .catch(error => next(error))
+    .then(response => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if(body.name === undefined){
-      return response.status(400).json({
-          error: 'name missing'
-      })
+    return response.status(400).json({
+      error: 'name missing'
+    })
   }
   const person = new Contact({
-      name: body.name,
-      number: body.number
+    name: body.name,
+    number: body.number
   })
   person.save()
-  .then(savedPerson => {
+    .then(savedPerson => {
       response.json(savedPerson)
-  })
-  .catch(error => next(error))
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const {name,number} = request.body
+  const { name,number } = request.body
 
-  const person = {
-      name: body.name,
-      number: body.number,
-  }
-  Contact.findByIdAndUpdate(request.params.id, 
-    {name, number}, 
-    {new: true, runValidators: true, context:'query'})
-      .then(updatedPerson => {
-          response.json(updatedPerson)
-          console.log(updatedPerson)
-      })
-      .catch(error => next(error))
+  Contact.findByIdAndUpdate(request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context:'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+      console.log(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -172,9 +121,9 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if(error.name === 'CastError') {
-      return response.status(400).send({error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
@@ -182,5 +131,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -6,33 +6,33 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Contact = mongoose.model('Contact', contactSchema)
 
-if(process.argv.length == 3){
-    Contact.find({}).then(result => {
-        result.forEach(contact => {
-            console.log(`${contact.name} ${contact.number}`)
-        })
-        mongoose.connection.close()
+if(process.argv.length === 3){
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+      console.log(`${contact.name} ${contact.number}`)
     })
-} else if (process.argv.length == 5){
+    mongoose.connection.close()
+  })
+} else if (process.argv.length === 5){
 
-    const contact = new Contact({
-        name: process.argv[3],
-        number: process.argv[4],
-    })
-    
-    contact.save().then(result => {
-        console.log(`${result.name} with number ${result.number} saved to contacts`)
-        mongoose.connection.close()
-    })
+  const contact = new Contact({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+
+  contact.save().then(result => {
+    console.log(`${result.name} with number ${result.number} saved to contacts`)
+    mongoose.connection.close()
+  })
 } else {
-    console.log('Usage: node mongo.js <password> <name> <phone_number>')
-    process.exit(1)
+  console.log('Usage: node mongo.js <password> <name> <phone_number>')
+  process.exit(1)
 }
 
 
